f7abba120eb1cad06fe4fc27814f8aaed0124dcc
nitishgovekar <nitishgovekar@192.168.0.104>
2020-06-13 11:58:48 +0530

    Implement PostMapping to create user

--- restfull/src/main/java/com/microservices/webservice/restfull/controller/UserController.java
+++ restfull/src/main/java/com/microservices/webservice/restfull/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.microservices.webservice.restfull.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.microservices.webservice.restfull.pojo.User;
 import com.microservices.webservice.restfull.pojo.UserDaoService;
@@ -30,5 +35,19 @@ public class UserController {
 
 		return userService.findById(id);
 	}
+	
+	//Insert Records
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = userService.save(user);
+		
+		//This will set response as Created and status code as 201
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}").
+				buildAndExpand(savedUser.getId()).toUri();
 
+		return ResponseEntity.created(location).build();
+
+	}
 }
