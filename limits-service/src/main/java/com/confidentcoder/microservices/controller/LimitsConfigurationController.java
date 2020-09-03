681297661d11ecb2fc711c9504cee34ee5948d7f
nitish.govekar <nitish.govekar>
2020-09-03 09:06:37 +0530

    updated code to enhance limits service to get configuration from application.properties

--- limits-service/src/main/java/com/confidentcoder/microservices/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/confidentcoder/microservices/controller/LimitsConfigurationController.java
@@ -1,17 +1,22 @@
 package com.confidentcoder.microservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.confidentcoder.microservices.beans.Configuration;
 import com.confidentcoder.microservices.beans.LimitsConfiguration;
 
 @RestController
 
 public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
 
 	@GetMapping("/limits")
 	public LimitsConfiguration  retrieveLimitsConfigurations() {
 		
-		return new LimitsConfiguration(1000, 1);
+		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
